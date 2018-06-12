8bc1bbc0ef36fbacf88080e632cc91113274d3b7
Jannes Brunner <jannes.brunner@student.htw-berlin.de>
2018-06-12 16:49:42 +0100

    Create addon.setup.php

---
+++ addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'author' => 'Jannes Brunner',
+    'author_url' => 'http://jannesbrunner.de',
+    'description' => 'Create instagram feed',
+    'name' => 'Instagram Feed',
+    'settings_exist' => false,
+    'version' => '1.0',
+    'namespace'   => 'JB\Instagram Feed'
+);
